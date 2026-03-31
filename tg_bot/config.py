fcb72e572c3f530682bb66b8400a3e4b962d88fe
afgoll <aflahmonuzz@gmail.com>
2026-03-31 23:27:41 +0530

    Update config.py

--- tg_bot/config.py
+++ tg_bot/config.py
@@ -1,10 +1,10 @@
 from tg_bot.sample_config import Development as Config
 
 class Config(Config):
-    API_KEY = "your_api_id"
-    OWNER_ID = 123456789
-    OWNER_USERNAME = "your_username"
+    API_KEY = "26149781"
+    OWNER_ID = 6272165202
+    OWNER_USERNAME = "@XAVIER_Tgx"
 
-    SQLALCHEMY_DATABASE_URI = "your_database_url"
+    SQLALCHEMY_DATABASE_URI = "mongodb+srv://Moovieee:Aflahmmthhhh128544@cluster0.efyyavr.mongodb.net/?appName=Cluster0"
 
-    SUDO_USERS = [123456789]
+    SUDO_USERS = [8199195480]
