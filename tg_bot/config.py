8a74d3c9ffc484177e13599b81f03242f2a36484
afgoll <aflahmonuzz@gmail.com>
2026-03-31 23:12:52 +0530

    Update config.py

--- tg_bot/config.py
+++ tg_bot/config.py
@@ -1,1 +1,10 @@
+from tg_bot.sample_config import Development as Config
 
+class Config(Config):
+    API_KEY = "your_api_id"
+    OWNER_ID = 123456789
+    OWNER_USERNAME = "your_username"
+
+    SQLALCHEMY_DATABASE_URI = "your_database_url"
+
+    SUDO_USERS = [123456789]
